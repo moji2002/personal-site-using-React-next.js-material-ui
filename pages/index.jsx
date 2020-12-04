59fb4accddeeab7976c5f5810d4d1a31ae4f4da7
Mojtaba <moji2002@gmail.com>
2020-12-04 15:17:54 +0330

    REMOVE UNUSED FILES

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,39 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../components/layout';
-import utilStyles from '../styles/utils.module.scss';
-import { getSortedPostsData } from '../lib/posts';
-import Date from '../components/date';
 
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
-    <Layout home>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}></section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <h1>hello world</h1>
   );
-}
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData
-    }
-  };
 }
